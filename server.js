276e2c775f7a4dc0284cc2c82cf087f5b6a96d06
Amir Tosson <45110899+amirtosson@users.noreply.github.com>
2022-12-20 12:26:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const express = require('express');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  accessKeyId: "AKIA6ENB2UFPJAG7WPEL",
+  secretAccessKey: "AKotqlHarfWCokk7NpmfwOPlY0qWTcSbozakNYiY"
+});
 
+var params = {
+  Bucket: 'daphne-angular'  // Can be your folder name
+};
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello DAPHNE !!');
+app.get('/aws/test', (req, res) => {
+  s3.listObjects(params, function(err, data) {
+    console.log("HERE")
+    if (err) {
+      console.log('There was an error viewing your album: ' + err.message)
+    } else {
+      console.log(data.Contents,"<<<all content");
+                  
+      res.send(data.Contents)    }
+  })
+  //res.send('Hello DAPHNE !!');
 });
 
 app.listen(PORT, HOST, () => {
