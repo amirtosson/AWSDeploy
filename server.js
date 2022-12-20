2552496da4e275d40b9fe084a3eecc720e1a0570
Amir Tosson <45110899+amirtosson@users.noreply.github.com>
2022-12-20 11:15:48 +0100

    cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const express = require('express');
-
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://daphne-angular.s3-website.eu-central-1.amazonaws.com/'
+}));
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
